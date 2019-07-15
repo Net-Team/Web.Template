8529e510f4edc2ba4b00114bd39235433d565271
xljiulang <366193849@qq.com>
2019-07-15 16:01:07 +0800

    合并Extension目录

--- Common/System/Data/CsvExtensions.cs
+++ Common/System/Data/CsvExtensions.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace System.Data
 {
     /// <summary>
     /// csv扩展
     /// </summary>
-    public static class CsvExtension
+    public static class CsvExtensions
     {
         /// <summary>
         /// 转换为Csv

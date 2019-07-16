0ec6a24d0ac93b75e908834bbe02dd9f0faa22ee
林松斌 <linsongbin@126.com>
2019-07-16 13:08:11 +0800

    冲突

---
+++ Domain/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// 定义实体的接口
+    /// </summary>
+    public interface IEntity : IStringId
+    {
+        /// <summary>
+        /// 最近更新时间
+        /// </summary>
+        DateTime LastModifyTime { get; set; }
+    }
+}
---
+++ Domain/IStringId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// 定义支持string类型的Id属性
+    /// </summary>
+    public interface IStringId
+    {
+        /// <summary>
+        /// 获取或设置唯一标识
+        /// </summary>
+        string Id { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        DateTime CreateTime { get; set; }
+    }
+}

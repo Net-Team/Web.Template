f6d6d4734947ede6977508444cd51634c4b30d01
xljiulang <366193849@qq.com>
2019-07-24 23:17:25 +0800

    使用TinyMapper实现最基础的Map功能

---
+++ Core/IMap.cs
@@ -0,0 +1,28 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// 定义表示映射体的接口
+    /// 提供对相同名称的属性进行映射
+    /// </summary>
+    /// <typeparam name="TMap"></typeparam>
+    public interface IMap<TMap>
+    {
+        /// <summary>
+        /// 映射到目标对象
+        /// 要求destination为public修饰
+        /// </summary>
+        /// <typeparam name="TDestination"></typeparam>
+        /// <param name="destination">目标对象</param>
+        /// <returns></returns>
+        TDestination To<TDestination>(TDestination destination);
+
+        /// <summary>
+        /// 从其它对象映射过来
+        /// 要求source为public修饰
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source">来源</param>
+        /// <returns></returns>
+        TMap From<TSource>(TSource source);
+    }
+}
---
+++ Core/MapExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Nelibur.ObjectMapper;
+
+namespace Core
+{
+    /// <summary>
+    /// 提供映射扩展
+    /// </summary>
+    public static class MapExtensions
+    {
+        /// <summary>
+        /// 转换为可映射对象
+        /// 要求对象为public修饰
+        /// </summary>
+        /// <typeparam name="TMap"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static IMap<TMap> AsMap<TMap>(this TMap value) where TMap : class
+        {
+            return new Map<TMap>(value);
+        }
+
+        /// <summary>
+        /// 提供TinyMapper的静态初始化与映射操作
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TDestination"></typeparam>
+        private static class Mapper<TSource, TDestination>
+        {
+            /// <summary>
+            /// 初始化绑定
+            /// </summary>
+            static Mapper()
+            {
+                TinyMapper.Bind<TSource, TDestination>();
+            }
+
+            /// <summary>
+            /// 映射对象
+            /// </summary>
+            /// <param name="source"></param>
+            /// <param name="destination"></param>
+            /// <returns></returns>
+            public static TDestination Map(TSource source, TDestination destination)
+            {
+                return TinyMapper.Map(source, destination);
+            }
+        }
+
+        /// <summary>
+        /// IMap的默认实现者
+        /// </summary>
+        /// <typeparam name="TMap"></typeparam>
+        private class Map<TMap> : IMap<TMap>
+        {
+            private readonly TMap map;
+
+            public Map(TMap map)
+            {
+                this.map = map;
+            }
+
+            /// <summary>
+            /// 从其它对象映射过来
+            /// 要求source为public修饰
+            /// </summary>
+            /// <typeparam name="TSource"></typeparam>
+            /// <param name="source">来源</param>
+            /// <returns></returns>
+            public TMap From<TSource>(TSource source)
+            {
+                return Mapper<TSource, TMap>.Map(source, this.map);
+            }
+
+            /// <summary>
+            /// 映射到目标对象
+            /// 要求destination为public修饰
+            /// </summary>
+            /// <typeparam name="TDestination"></typeparam>
+            /// <param name="destination">目标对象</param>
+            /// <returns></returns>
+            public TDestination To<TDestination>(TDestination destination)
+            {
+                return Mapper<TMap, TDestination>.Map(this.map, destination);
+            }  
+        }
+    }
+}

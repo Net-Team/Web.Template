9f4b9cef37fdcd2ff4cdb8c72f9d7b6d5680c960
xljiulang <366193849@qq.com>
2019-09-10 11:21:35 +0800

    更新到prev9

--- Corefx/Linq/IEnumerableExtensions.cs
+++ Corefx/Linq/IEnumerableExtensions.cs
@@ -41,5 +41,64 @@ namespace System.Linq
                 item(value);
             }
         }
+
+        /// <summary>
+        /// 以数字分段的排序
+        /// </summary>
+        /// <typeparam name="TRoom"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="nameSelector">名称选择器</param>
+        /// <returns></returns>
+        public static IEnumerable<TRoom> OrderByNumericSegments<TRoom>(this IEnumerable<TRoom> source, Func<TRoom, string> nameSelector)
+        {
+            if (source == null)
+            {
+                return null;
+            }
+
+            return source
+                .Select(item => new Segment<TRoom>(item, nameSelector(item)))
+                .OrderBy(item => item.SegmentCount)
+                .ThenBy(item => item.SegmentValue)
+                .Select(item => item.Data);
+        }
+
+        /// <summary>
+        /// 表示结构分段
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class Segment<T>
+        {
+            /// <summary>
+            /// 获取原始数据
+            /// </summary>
+            public T Data { get; private set; }
+
+            /// <summary>
+            /// 获取分段数量
+            /// </summary>
+            public int SegmentCount { get; private set; }
+
+            /// <summary>
+            /// 获取分段值
+            /// </summary>
+            public ulong SegmentValue { get; private set; }
+
+            /// <summary>
+            /// 项目的结构分段
+            /// </summary>
+            /// <param name="data">数据</param>
+            /// <param name="name">名称</param>
+            public Segment(T data, string name)
+            {
+                this.Data = data;
+                var segments = name.Matches(@"\d+(?=\D)");
+                this.SegmentCount = segments.Length;
+                if (segments.Length > 0)
+                {
+                    this.SegmentValue = ulong.Parse(string.Join(string.Empty, segments));
+                }
+            }
+        }
     }
 }

1b2a47934d0f3bbabd6368c948912d5ac66005ee
xljiulang <366193849@qq.com>
2019-12-02 13:36:50 +0800

    增加Distinct扩展

--- Corefx/Linq/IEnumerableExtensions.cs
+++ Corefx/Linq/IEnumerableExtensions.cs
@@ -8,6 +8,44 @@ namespace System.Linq
     public static class IEnumerableExtensions
     {
         /// <summary>
+        /// 去除重复数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
+        public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            return source.Distinct(keySelector, EqualityComparer<TKey>.Default);
+        }
+
+        /// <summary>
+        /// 去除重复数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="keyComparer"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns></returns>
+        public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector, IEqualityComparer<TKey> keyComparer)
+        {
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException(nameof(keySelector));
+            }
+            if (keyComparer == null)
+            {
+                throw new ArgumentNullException(nameof(keyComparer));
+            }
+            var comparer = new KeyEqualityComparer<T, TKey>(keySelector, keyComparer);
+            return source.Distinct(comparer);
+        }
+
+        /// <summary>
         /// 为Null则返回0条记录的迭代
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -61,6 +99,33 @@ namespace System.Linq
                 .OrderBy(item => item.SegmentCount)
                 .ThenBy(item => item.SegmentValue)
                 .Select(item => item.Data);
+        }
+
+        /// <summary>
+        /// 键选择比较器
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        private class KeyEqualityComparer<T, TKey> : IEqualityComparer<T>
+        {
+            private readonly Func<T, TKey> keySelector;
+            private readonly IEqualityComparer<TKey> comparer;
+
+            public KeyEqualityComparer(Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer)
+            {
+                this.keySelector = keySelector;
+                this.comparer = comparer;
+            }
+
+            public bool Equals(T x, T y)
+            {
+                return comparer.Equals(keySelector(x), keySelector(y));
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return comparer.GetHashCode(keySelector(obj));
+            }
         }
 
         /// <summary>
---
+++ Web.Test/Corefx/IEnumerableExtensionsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Web.Test.Corefx
+{
+    public class IEnumerableExtensionsTest
+    {
+        class A
+        {
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void Distinct()
+        {
+            var a = new A { Name = "a" };
+            var b = new A { Name = "a" };
+            var c = new A { Name = "c" };
+
+            var v = new[] { a, b, c }.Distinct(item => item.Name).ToArray();
+            Assert.True(v.Length == 2);
+            Assert.True(v.Last().Name == "c");
+        }
+    }
+}

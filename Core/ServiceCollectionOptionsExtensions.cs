803c564d41576a9b39e460fd4c6e46c107c23a2a
陈国伟 <366193849@qq.com>
2020-09-08 13:06:07 +0800

    使用泛型类替代泛型方法反射调用

--- Core/ServiceCollectionOptionsExtensions.cs
+++ Core/ServiceCollectionOptionsExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Reflection;
 
 namespace Core
@@ -21,21 +20,51 @@ namespace Core
         /// <param name="configuration"></param>
         public static IServiceCollection AddConfigureOptions(this IServiceCollection services, IConfiguration configuration, params Assembly[] assemblies)
         {
-            const string name = nameof(OptionsConfigurationServiceCollectionExtensions.Configure);
-            var method = typeof(OptionsConfigurationServiceCollectionExtensions)
-                .GetMethod(name, 1, new[] { typeof(IServiceCollection), typeof(IConfiguration) });
-
             foreach (var assembly in assemblies)
             {
-                var optionsTypes = assembly.GetTypes().Where(item => item.IsInheritFrom<IConfigureOptions>());
-                foreach (var type in optionsTypes)
+                foreach (var optionsType in assembly.GetTypes())
                 {
-                    var configSection = configuration.GetSection(type.Name);
-                    method.MakeGenericMethod(type).Invoke(null, new object[] { services, configSection });
+                    if (optionsType.IsInheritFrom<IConfigureOptions>() == true)
+                    {
+                        var configSection = configuration.GetSection(optionsType.Name);
+                        services.Bind(optionsType, configSection);
+                    }
                 }
             }
-
             return services;
+        }
+
+        /// <summary>
+        /// 绑定配置到选项
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="optionsType"></param>
+        /// <param name="configuration"></param>
+        private static void Bind(this IServiceCollection services, Type optionsType, IConfiguration configuration)
+        {
+            var builderType = typeof(BindingOptionsBuilder<>).MakeGenericType(optionsType);
+            var builder = (IBindingOptionsBuilder)Activator.CreateInstance(builderType, services);
+            builder.Bind(configuration);
+        }
+
+        private interface IBindingOptionsBuilder
+        {
+            void Bind(IConfiguration configureOptions);
+        }
+
+        private class BindingOptionsBuilder<T> : IBindingOptionsBuilder where T : class
+        {
+            private readonly OptionsBuilder<T> builder;
+
+            public BindingOptionsBuilder(IServiceCollection services)
+            {
+                this.builder = new OptionsBuilder<T>(services, Options.DefaultName);
+            }
+
+            public void Bind(IConfiguration configureOptions)
+            {
+                this.builder.Bind(configureOptions);
+            }
         }
     }
 }
